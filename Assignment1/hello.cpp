e478cb4355ff2b50e3ca7f172c1f31e33b6bc6dd
Алексей Пащенко <st141278@student.spbu.ru>
2025-10-01 16:40:17 +0300

    Added a loop and a greeting function

--- Assignment1/hello.cpp
+++ Assignment1/hello.cpp
@@ -4,4 +4,16 @@
 
 void myproject::sayHello() {
     std::cout << "Hello, world!" << std::endl;
+
+    std::string input;
+    
+    std::cout << "Write the name (or 'exit' to end the program): ";
+    std::getline(std::cin, input);  // reading the string before the loop to check the exit condition
+
+    while (input != "exit") {                
+        std::cout << "Hello, " << input << "!" << std::endl;
+
+        std::cout << "Write the name (or 'exit' to end the program): ";
+        std::getline(std::cin, input);
+    }
 }
