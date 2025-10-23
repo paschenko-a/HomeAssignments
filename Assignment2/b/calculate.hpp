cf1f330be87b5c2e532376a856228576e50b4242
Алексей Пащенко <st141278@student.spbu.ru>
2025-10-24 01:50:42 +0300

    added structure for Assignment2b

---
+++ Assignment2/b/calculate.hpp
@@ -0,0 +1,9 @@
+/*
+* Pashchenko Alexey
+* st141278@student.spbu.ru
+* My project number two (b)
+*/
+#ifndef CALCULATE_HPP
+#define CALCULATE_HPP
+
+#endif
---
+++ Assignment2/b/main.cpp
@@ -0,0 +1,8 @@
+/*
+* Pashchenko Alexey
+* st141278@student.spbu.ru
+* My project number two (b)
+*/
+
+#include "calculate.hpp"
+
